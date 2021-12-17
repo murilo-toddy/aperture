64a08a968b0940a1bd17ce66ac253d350c1d3f6f
murilo-toddy <murilo.todao@usp.br>
2021-12-16 23:27:26 -0300

    Better organizes windows

--- src/app/window.py
+++ src/app/window.py
@@ -22,7 +22,31 @@ class WindowHandler:
         
         if self.__debug:
             print("Established connection with Glade")
+
+        self.__login_window_name = "login_window"
+        self.__home_window_name = "home_window"
+        self.__error_window_name = "error_dialog"
+        self.__home_window_name = "home_window"
+        self.__updateinfo_window_name = "updateinfo_window"
+        self.__criticalerror_window_name = "criticalerror_window"
+
+
+    def __update_obj(self): self.obj = self.builder.get_object
+
+
+    def __show_window(self, window_name: str):
+        self.__update_obj()
+        if self.__debug:
+            print(f"Opening {window_name}")
         
+        self.obj(window_name).show_all()
+
+
+    def __hide_window(self, window_name: str):
+        if self.__debug:
+            print(f"Hiding {window_name}")
+        self.obj(window_name).close()
+
 
     def load_default_config(self):
         query = "SELECT * FROM user_info"
@@ -40,40 +64,47 @@ class WindowHandler:
 
     """Login Window"""
     def show_login_window(self):
-        self.obj = self.builder.get_object
-        self.obj("login_window").show_all()
-        if self.__debug:
-            print("Oppening login window")
-
+        self.__show_window(self.__login_window_name)
 
     def hide_login_window(self):
-        if self.__debug:
-            print("Closing login window")
-        self.builder.get_object("login_window").close()
-        self.obj("login_window").close()
-
+        self.__hide_window(self.__login_window_name)
 
     def get_login_info(self):
         return self.obj("login_name").get_text(), self.obj("login_surname").get_text(), self.obj("login_birthday").get_text()
 
+    def get_update_info(self):
+        return self.obj("update_name").get_text(), self.obj("update_surname").get_text(), self.obj("update_birthday").get_text()
+
 
     """Error Window"""
     def show_error_window(self):
-        if self.__debug:
-            print("Opening error window")
-        self.obj("error_dialog").show_all()
-
+        self.__show_window(self.__error_window_name)
 
     def close_error_window(self):
-        if self.__debug:
-            print("Closing error window")
-        self.obj("error_dialog").close()
+        self.__hide_window(self.__error_window_name)
+
+    
+    """Critical Error Window"""
+    def show_criticalerror_window(self):
+        self.__show_window(self.__criticalerror_window_name)
+
+    def hide_criticalerror_window(self):
+        self.__hide_window(self.__criticalerror_window_name)
 
 
     """Home Window"""
     def show_home_window(self):
-        if self.__debug:
-            print("Opening home window")
-        self.obj = self.builder.get_object
-        self.obj("home_window").show_all()
+        self.__show_window(self.__home_window_name)
         self.load_default_config()
+
+    def hide_home_window(self):
+        self.__hide_window(self.__home_window_name)
+
+
+    """Update Info Window"""
+    def show_updateinfo_window(self):
+        self.__show_window(self.__updateinfo_window_name)
+
+    def hide_updateinfo_window(self):
+        self.__hide_window(self.__updateinfo_window_name)
+        
